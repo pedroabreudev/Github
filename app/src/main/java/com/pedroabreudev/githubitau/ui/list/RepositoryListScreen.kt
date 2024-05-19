18da772c17c209d57e70d841f879feeb6a0fd786
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2024-05-19 11:26:30 -0400

    remove imports.

--- app/src/main/java/com/pedroabreudev/githubitau/ui/list/RepositoryListScreen.kt
+++ app/src/main/java/com/pedroabreudev/githubitau/ui/list/RepositoryListScreen.kt
@@ -1,11 +1,8 @@
 package com.pedroabreudev.githubitau.ui.list
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
